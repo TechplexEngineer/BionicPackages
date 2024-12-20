c6b0a9103b63c8df4fcc3336c54b6cbc71727cf0
Blake Bourque <techplex.engineer@gmail.com>
2024-12-20 13:41:21 -0500

    Now will it build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	optimizeDeps: { exclude: ["fsevents"] },
+	optimizeDeps: {
+		exclude: [
+			"fsevents",
+			"@node-rs/argon2",
+			"@node-rs/bcrypt",
+			"@node-rs/argon2-wasm32-wasi"
+		]
+	},
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
