61724c962a704c9932a41275849cb727def99696
Blake Bourque <techplex.engineer@gmail.com>
2024-12-25 06:35:16 -0500

    Green: trackEvent

--- src/lib/service/tracking.ts
+++ src/lib/service/tracking.ts
@@ -9,7 +9,12 @@ export class TrackingService {
 
     // This method should be called whenever a tracking event is received from the carrier
     trackEvent(trackingNumber: string, event: { status: string, estimatedDelivery: string, trackingUrl: string, latestUpdate: string }) {
-
+        return this.db.update(packagesTable).set({
+            tracking: event
+        }).where(and(
+            eq(packagesTable.trackingNumber, trackingNumber),
+            eq(packagesTable.tennant, this.tennant)
+        ));
     }
 
     async addPackage(props: { name: string, trackingNumber: string, carrier: string }) {
