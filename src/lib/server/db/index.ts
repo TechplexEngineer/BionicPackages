d4f383a23c165226caf92b231168e8a7685d5994
Blake Bourque <techplex.engineer@gmail.com>
2024-12-27 12:38:26 -0500

    Get local dev working after publishing

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,19 +1,21 @@
 import { drizzle as drizzle_libsql } from 'drizzle-orm/libsql';
-import {drizzle as drizzle_d1} from "drizzle-orm/d1";
+import { drizzle as drizzle_d1, DrizzleD1Database } from "drizzle-orm/d1";
 import { createClient, type ResultSet } from '@libsql/client';
 import { env } from '$env/dynamic/private';
 import { type BaseSQLiteDatabase } from 'drizzle-orm/sqlite-core/db';
 import type { D1Result } from '@miniflare/d1';
 
-export type Database = BaseSQLiteDatabase<'async', D1Result | ResultSet>;
+export type Database = DrizzleD1Database
 
 
 
-export const getDb = async (platform: App.Platform): Promise<Database> => {
-    if (platform.env?.bionic_packages_db) {
-        return drizzle_d1(platform.env?.bionic_packages_db)
+export const getDb = async (platform: App.Platform) => {
+    if (!platform.env?.bionic_packages_db) {
+        throw new Error("database binding not set: 'bionic_packages_db'")
     }
-    if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-    const client = createClient({ url: env.DATABASE_URL });
-    return drizzle_libsql(client);
+    return drizzle_d1(platform.env?.bionic_packages_db)
+
+    // if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+    // const client = createClient({ url: env.DATABASE_URL });
+    // return drizzle_libsql(client);
 }
--- src/lib/startup.ts
+++ src/lib/startup.ts
@@ -5,22 +5,21 @@ export const startup = async (platform: App.Platform): Promise<Omit<App.Locals, 
     console.log("Startup");
 
     const db = await getDb(platform);
-    if (platform.env?.bionic_packages_db) {
-        // use d1
-        console.log("Migrations need to be applied by wrangler manually");
-    } else {
-        if(import.meta.env.DEV) {
-            const { migrate: migrate_libsql } = await import('drizzle-orm/libsql/migrator');
-            // use libsql
-            await migrate_libsql(db, {
-                migrationsFolder: "./drizzle",
-                migrationsSchema: './src/lib/server/db/schema.ts',
-                migrationsTable: "__drizzle_migrations",
 
-            });
-        }
-    }
-    console.log("Migrations Complete");
+    console.log("REMINDER: Migrations need to be applied by wrangler manually");
+    // if (!import.meta.env.DEV) {
+
+    // } else {
+    //     const { migrate } = await import('drizzle-orm/d1/migrator');
+    //     // use libsql
+    //     await migrate(db, {
+    //         migrationsFolder: "./drizzle",
+    //         migrationsSchema: './src/lib/server/db/schema.ts',
+    //         migrationsTable: "__drizzle_migrations",
+
+    //     });
+    //     console.log("Migrations Complete");
+    // }
 
     return {
         db: db
