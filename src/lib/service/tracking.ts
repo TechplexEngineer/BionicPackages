b5c77c9c5c5d088c1c8b91c8ad6feb96212218e0
Blake Bourque <techplex.engineer@gmail.com>
2025-01-01 09:52:06 -0500

    Maybe its throwing

--- src/lib/service/tracking.ts
+++ src/lib/service/tracking.ts
@@ -33,62 +33,67 @@ export class TrackingService {
 
     async addPackage(props: { name: string, trackingNumber: string, carrier: string }) {
 
-        // Create a tracker with easypost
-        console.log("Creating tracker with easypost");
-        const ep = new EasyPost(EASYPOST_API_KEY);
-        const tracker = await ep.Tracker.create(props.trackingNumber, props.carrier);
-        if ('error' in tracker) {
-            console.log("Error creating tracker with easypost", tracker.error);
-            throw new Error(tracker.error?.message);
-        }
-        console.log("Tracker created", tracker);
+        try {
+            // Create a tracker with easypost
+            console.log("Creating tracker with easypost");
+            const ep = new EasyPost(EASYPOST_API_KEY);
+            const tracker = await ep.Tracker.create(props.trackingNumber, props.carrier);
+            if ('error' in tracker) {
+                console.log("Error creating tracker with easypost", tracker.error);
+                throw new Error(tracker.error?.message);
+            }
+            console.log("Tracker created", tracker);
 
-        // store the update
-        await this.db.insert(packagesTable).values({
-            name: props.name,
-            trackingNumber: props.trackingNumber,
-            carrier: props.carrier,
-            tenant: this.tenant,
-            tracking: {
+            // store the update
+            await this.db.insert(packagesTable).values({
+                name: props.name,
+                trackingNumber: props.trackingNumber,
+                carrier: props.carrier,
+                tenant: this.tenant,
+                tracking: {
+                    status: tracker.status,
+                    estimatedDelivery: tracker.est_delivery_date,
+                    trackingUrl: tracker.public_url,
+                    latestUpdate: tracker.tracking_details[0].message,
+                }
+            });
+            console.log("Package added to database");
+
+            // check if the webhook is redundant
+
+
+            // find slack connection for tenant
+            const tenantSlackConnection = await this.db.select().from(tenantTable).where(eq(tenantTable.teamId, this.tenant)).limit(1);
+            if (tenantSlackConnection.length <= 0) {
+                console.log(`No slack connection found for tenant: ${this.tenant}`);
+                throw new Error("Slack connection not configured"); // user visible message
+            }
+            console.log("Slack connection found");
+            
+            // send message to slack
+            const slackMessage = formatTrackingSlackMessage({
+                name: props.name,
+                trackingNumber: props.trackingNumber,
                 status: tracker.status,
                 estimatedDelivery: tracker.est_delivery_date,
                 trackingUrl: tracker.public_url,
-                latestUpdate: tracker.tracking_details[0].message,
+                latestUpdate: tracker.tracking_details[0].message
+            });
+            const url = tenantSlackConnection[0].data?.incoming_webhook.url;
+            if (!url) {
+                console.log(`mising slack webhook url for tenant: ${this.tenant}`);
+                throw new Error("Slack connection not configured");
             }
-        });
-        console.log("Package added to database");
-
-        // check if the webhook is redundant
-
-
-        // find slack connection for tenant
-        const tenantSlackConnection = await this.db.select().from(tenantTable).where(eq(tenantTable.teamId, this.tenant)).limit(1);
-        if (tenantSlackConnection.length <= 0) {
-            console.log(`No slack connection found for tenant: ${this.tenant}`);
-            throw new Error("Slack connection not configured"); // user visible message
-        }
-        console.log("Slack connection found");
-        
-        // send message to slack
-        const slackMessage = formatTrackingSlackMessage({
-            name: props.name,
-            trackingNumber: props.trackingNumber,
-            status: tracker.status,
-            estimatedDelivery: tracker.est_delivery_date,
-            trackingUrl: tracker.public_url,
-            latestUpdate: tracker.tracking_details[0].message
-        });
-        const url = tenantSlackConnection[0].data?.incoming_webhook.url;
-        if (!url) {
-            console.log(`mising slack webhook url for tenant: ${this.tenant}`);
-            throw new Error("Slack connection not configured");
-        }
-        console.log("Sending message to slack");
-        await fetch(url, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ text: slackMessage })
-        });        
+            console.log("Sending message to slack");
+            await fetch(url, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ text: slackMessage })
+            }); 
+        } catch (e) {
+            console.error("Error adding package", e);
+            throw new Error("Error adding package");
+        }       
     }
 
     listPackages() {
